66fb56db844515522b898eca54372e8cf09853e5
Ashita Airan <ashitaairan@Ashitas-MacBook-Air.local>
2026-06-14 18:14:19 +0530

    replace tavily with duckduckgo search

--- tools.py
+++ tools.py
@@ -1,26 +1,22 @@
 from langchain.tools import tool 
 import requests
 from bs4 import BeautifulSoup
-from tavily import TavilyClient
+from duckduckgo_search import DDGS
 import os 
 from dotenv import load_dotenv
 from rich import print
 load_dotenv()
 
-tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
-
 @tool
 def web_search(query : str) -> str:
-    """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
-    results = tavily.search(query=query,max_results=5)
-
+    """Search the web for recent and reliable information on a topic. Returns Titles, URLs and snippets."""
     out = []
-
-    for r in results['results']:
+    with DDGS() as ddgs:
+        results = list(ddgs.text(query, max_results=5))
+    for r in results:
         out.append(
-            f"Title: {r['title']}\nURL: {r['url']}\nSnippet: {r['content'][:300]}\n"
+            f"Title: {r['title']}\nURL: {r['href']}\nSnippet: {r['body'][:300]}\n"
         )
-    
     return "\n----\n".join(out)
 
 @tool
